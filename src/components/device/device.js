9a6636fd34207ac0b2b45c0fefd767dad60679c9
Paweł Wrzosek <wrzonki@gmail.com>
2020-06-14 14:58:56 +0200

    added buttons

--- src/components/device/device.js
+++ src/components/device/device.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import s from './device.module.scss';
-const { useState } = React;
 
-export default () => {
-  const [left, setSide] = useState(true);
-  const clickHandler = () => {
-    setSide(!left);
-  };
+export default props => {
+  let left = props.side;
+  let screen = props.screen
+  console.log(props)
   return (
-    <div role="button" tabIndex={0} className={s.wrapper} onClick={clickHandler} onKeyDown={clickHandler}>
-      <div className={`${s.device} ${left ? s.side_left : s.side_right} `}>
-        <div className={`${s.triangle} ${left ? s.triangle_left : s.triangle_right}`}></div>
+    <div className={`${s.wrapper} ${left ? s.left : s.right}`}>
+      <div className={`${s.device} ${left ? s.side_right : s.side_left}`}>
+        <div className={s.shine}></div>
         <div className={s.frame}>
           <div className={s.camera}>
             <div className={s.dot}></div>
           </div>
-          <div className={`${s.shine} ${s.shine_left}`}></div>
-          <div className={`${s.shine} ${s.shine_right}`}></div>
-          <div className={s.contents}>
+          <div className={`${s.curve} ${s.curve_left}`}></div>
+          <div className={`${s.curve} ${s.curve_right}`}></div>
+          <div className={`${s.contents} ${screen ? s.hide : s.show}`}>
             <div className={s.clock}>12:17</div>
             <div className={s.tel}></div>
             <div className={s.photo}></div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import '../styles/reset.scss';
 import '../styles/global.scss';
+
 import Section from '../components/section/section';
 import Device from '../components/device/device';
 
 export default function Home() {
+  const [isLeft, setSide] = useState(false);
+  const toggleSide = () => setSide(!isLeft);
+
+  const [isScreen, setScreen] = useState(false);
+  const toggleScreen = () => setScreen(!isScreen);
+
+  var bt1 = {
+    position: 'absolute',
+    top: '100px',
+    left: '100px'
+  }
+
+  var bt2 = {
+    position: 'absolute',
+    top: '130px',
+    left: '100px'
+  }
+
   return (
-    <Section>
-      <Device></Device>
-    </Section>
+    <>
+    {/* <Shine/> */}
+      <Device side={isLeft} screen={isScreen}></Device>
+      <Section>
+        <button aria-label="switch side" onClick={toggleSide} style={bt1}>
+          switch
+        </button>
+        <button aria-label="switch side" onClick={toggleScreen} style={bt2}>
+          screen
+        </button>
+        
+      </Section>
+    </>
   );
 }
