86518e438136ed9cf97db3606c777a921b23eee7
Paweł Wrzosek <wrzonki@gmail.com>
2020-06-21 15:32:24 +0200

    yo

--- src/components/device/device.js
+++ src/components/device/device.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState  } from 'react';
 import s from './device.module.scss';
 
 export default props => {
+
   let left = props.side;
-  let screen = props.screen
-  console.log(props)
+  let screen = props.screen;
+
   return (
     <div className={`${s.wrapper} ${left ? s.left : s.right}`}>
       <div className={`${s.device} ${left ? s.side_right : s.side_left}`}>
@@ -16,9 +17,6 @@ export default props => {
           <div className={`${s.curve} ${s.curve_left}`}></div>
           <div className={`${s.curve} ${s.curve_right}`}></div>
           <div className={`${s.contents} ${screen ? s.hide : s.show}`}>
-            <div className={s.clock}>12:17</div>
-            <div className={s.tel}></div>
-            <div className={s.photo}></div>
           </div>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,27 +7,27 @@ import Section from '../components/section/section';
 import Device from '../components/device/device';
 
 export default function Home() {
-  const [isLeft, setSide] = useState(false);
+  const [isLeft, setSide] = useState(true);
   const toggleSide = () => setSide(!isLeft);
 
-  const [isScreen, setScreen] = useState(false);
+  const [isScreen, setScreen] = useState(true);
   const toggleScreen = () => setScreen(!isScreen);
 
   var bt1 = {
     position: 'absolute',
     top: '100px',
-    left: '100px'
-  }
+    left: '100px',
+  };
 
   var bt2 = {
     position: 'absolute',
     top: '130px',
-    left: '100px'
-  }
+    left: '100px',
+  };
 
   return (
     <>
-    {/* <Shine/> */}
+      {/* <Shine/> */}
       <Device side={isLeft} screen={isScreen}></Device>
       <Section>
         <button aria-label="switch side" onClick={toggleSide} style={bt1}>
@@ -36,7 +36,6 @@ export default function Home() {
         <button aria-label="switch side" onClick={toggleScreen} style={bt2}>
           screen
         </button>
-        
       </Section>
     </>
   );
